61d17a22e90598d1c3d2ec470aebb8ff11834780
myru31 <36711263+myru31@users.noreply.github.com>
2020-10-17 16:04:24 -0500

    Added filtering by state. One problem is that multiple checkboxes can be selected but the program won't display multiple variables selectively

--- tamu_example.py
+++ tamu_example.py
@@ -17,16 +17,34 @@ st.write(df)
 
 # Try out filtering the data and using a checkbox
 
+# Create a list holding all states plus an "All" option
+state_list = list(df.State.unique())
+state_list = ["All"] + state_list
+
+# Make a dropdown select for states
+state_select = st.selectbox("State: ", state_list)
+
+# Filter data based on states and variable
 st.write("Filter the data: ")
+if state_select == "All":
+    if st.checkbox("Poverty"):
+        st.write(df[["State", "Geographic School District", "% Poverty (SAIPE Estimate)"]])
+    elif st.checkbox("Internet/Computer Access"):
+        st.write(df[["State", "Geographic School District", "% No Computer or Internet Estimate"]])
+    elif st.checkbox("Vulnerable Jobs"):
+        st.write(df[["State", "Geographic School District", "% HHs With Vulnerable Job Estimate"]])
+    elif st.checkbox("Single Parent Houshold"):
+        st.write(df[["State", "Geographic School District", "% Single Parent Estimate"]])
+else:
+    new_df = df[(df.State == state_select)]
+    if st.checkbox("Poverty"):
+        st.write(new_df[["State", "Geographic School District", "% Poverty (SAIPE Estimate)"]])
+    elif st.checkbox("Internet/Computer Access"):
+        st.write(new_df[["State", "Geographic School District", "% No Computer or Internet Estimate"]])
+    elif st.checkbox("Vulnerable Jobs"):
+        st.write(new_df[["State", "Geographic School District", "% HHs With Vulnerable Job Estimate"]])
+    elif st.checkbox("Single Parent Houshold"):
+        st.write(new_df[["State", "Geographic School District", "% Single Parent Estimate"]])
 
-if st.checkbox("Poverty"):
-    st.write(df[["State", "Geographic School District", "% Poverty (SAIPE Estimate)"]])
-elif st.checkbox("Internet/Computer Access"):
-    st.write(df[["State", "Geographic School District", "% No Computer or Internet Estimate"]])
-elif st.checkbox("Vulnerable Jobs"):
-    st.write(df[["State", "Geographic School District", "% HHs With Vulnerable Job Estimate"]])
-elif st.checkbox("Single Parent Houshold"):
-    st.write(df[["State", "Geographic School District", "% Single Parent Estimate"]])
+    
 
-# Filter by state
-# working on this

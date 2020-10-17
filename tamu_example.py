ae9d293d20237b35f28ac890b1aa37f4da5f6bd8
myru31 <36711263+myru31@users.noreply.github.com>
2020-10-17 17:57:11 -0500

    put data loading in separate function

--- tamu_example.py
+++ tamu_example.py
@@ -6,45 +6,51 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.title("TAMU Datathon")
+@st.cache
+def loadData():
+    return pd.read_excel("https://urban-data-catalog.s3.amazonaws.com/drupal-root-live/2020/06/08/NHGIS_District_data.xlsx")
 
-df = pd.read_excel("https://urban-data-catalog.s3.amazonaws.com/drupal-root-live/2020/06/08/NHGIS_District_data.xlsx")
+def main():
+    df = loadData()
+    
+    st.title("TAMU Datathon")
 
-st.write(
-    "Here is the data: "
-    )
-st.write(df)
+    st.write(
+        "Here is the data: "
+        )
+    st.write(df)
 
-# Try out filtering the data and using a checkbox
+    # Try out filtering the data and using a checkbox
 
 # Create a list holding all states plus an "All" option
-state_list = list(df.State.unique())
-state_list = ["All"] + state_list
+    state_list = list(df.State.unique())
+    state_list = ["All"] + state_list
 
 # Make a dropdown select for states
-state_select = st.selectbox("State: ", state_list)
+    state_select = st.selectbox("State: ", state_list)
 
 # Filter data based on states and variable
-st.write("Filter the data: ")
-if state_select == "All":
-    if st.checkbox("Poverty"):
-        st.write(df[["State", "Geographic School District", "% Poverty (SAIPE Estimate)"]])
-    if st.checkbox("Internet/Computer Access"):
-        st.write(df[["State", "Geographic School District", "% No Computer or Internet Estimate"]])
-    if st.checkbox("Vulnerable Jobs"):
-        st.write(df[["State", "Geographic School District", "% HHs With Vulnerable Job Estimate"]])
-    if st.checkbox("Single Parent Houshold"):
-        st.write(df[["State", "Geographic School District", "% Single Parent Estimate"]])
-else:
-    new_df = df[(df.State == state_select)]
-    if st.checkbox("Poverty"):
-        st.write(new_df[["State", "Geographic School District", "% Poverty (SAIPE Estimate)"]])
-    if st.checkbox("Internet/Computer Access"):
-        st.write(new_df[["State", "Geographic School District", "% No Computer or Internet Estimate"]])
-    if st.checkbox("Vulnerable Jobs"):
-        st.write(new_df[["State", "Geographic School District", "% HHs With Vulnerable Job Estimate"]])
-    if st.checkbox("Single Parent Houshold"):
-        st.write(new_df[["State", "Geographic School District", "% Single Parent Estimate"]])
+    st.write("Filter the data: ")
+    if state_select == "All":
+        if st.checkbox("Poverty"):
+            st.write(df[["State", "Geographic School District", "% Poverty (SAIPE Estimate)"]])
+        if st.checkbox("Internet/Computer Access"):
+            st.write(df[["State", "Geographic School District", "% No Computer or Internet Estimate"]])
+        if st.checkbox("Vulnerable Jobs"):
+            st.write(df[["State", "Geographic School District", "% HHs With Vulnerable Job Estimate"]])
+        if st.checkbox("Single Parent Houshold"):
+            st.write(df[["State", "Geographic School District", "% Single Parent Estimate"]])
+    else:
+        new_df = df[(df.State == state_select)]
+        if st.checkbox("Poverty"):
+            st.write(new_df[["State", "Geographic School District", "% Poverty (SAIPE Estimate)"]])
+        if st.checkbox("Internet/Computer Access"):
+            st.write(new_df[["State", "Geographic School District", "% No Computer or Internet Estimate"]])
+        if st.checkbox("Vulnerable Jobs"):
+            st.write(new_df[["State", "Geographic School District", "% HHs With Vulnerable Job Estimate"]])
+        if st.checkbox("Single Parent Houshold"):
+            st.write(new_df[["State", "Geographic School District", "% Single Parent Estimate"]])
 
-    
+main() 
+
 
